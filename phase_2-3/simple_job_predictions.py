435b7f8745f8b290e03b0a1a53d3ebdd7e161fa3
FebsN0 <fab.alt@protonmail.com>
2022-10-30 21:58:47 -0400

    fix

--- phase_2-3/simple_job_predictions.py
+++ phase_2-3/simple_job_predictions.py
@@ -51,7 +51,7 @@ for f in part_files:
               'w') as ref:
         ref.write('#!/bin/bash\n')
         ref.write('#SBATCH --account=def-jtus\n#SBATCH --nodes=1\n')
-        ref.write('#SBATCH --gres=gpu:a100:1     \#####  AWARE: MODIFY NAME GPU BASED ON THE USED CLUSTER \#####\n')
+        ref.write('#SBATCH --gres=gpu:a100:1     #####  AWARE: MODIFY NAME GPU BASED ON THE USED CLUSTER #####\n')
         ref.write('#SBATCH --cpus-per-task=10\n#SBATCH --mem-per-cpu=2G\n#SBATCH --time 05:00:00\n#SBATCH --ntasks=2\n')
         ref.write('#SBATCH --mail-user=fab.alt@protonmail.com\n#SBATCH --mail-type=BEGIN\n#SBATCH --mail-type=END\n#SBATCH --mail-type=FAIL\n')
         ref.write('source ~/.bashrc\n')
