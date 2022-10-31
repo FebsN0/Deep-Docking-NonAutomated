38b5191a5cb052e990980825937296856fa7c162
FebsN0 <72561622+FebsN0@users.noreply.github.com>
2022-10-30 20:09:11 -0600

    Update simple_job_models.py

--- phase_2-3/simple_job_models.py
+++ phase_2-3/simple_job_models.py
@@ -167,8 +167,8 @@ for i in range(len(all_hyperparas)):
 #personalized
 	ref.write('#SBATCH --account=def-jtus\n#SBATCH --nodes=1\n')
 	ref.write('#SBATCH --gres=gpu:a100:1     #####  AWARE: MODIFY NAME GPU BASED ON THE USED CLUSTER #####\n')
-	ref.write('#SBATCH --cpus-per-task=10\n#SBATCH --mem-per-cpu=2G\n#SBATCH --time 05:00:00\n#SBATCH --ntasks=2\n')
-	ref.write('#SBATCH --mail-user=fab.alt@protonmail.com\n#SBATCH --mail-type=BEGIN\n#SBATCH --mail-type=END\n#SBATCH --mail-type=FAIL\n')
+	ref.write('#SBATCH --cpus-per-task=5\n#SBATCH --mem-per-cpu=2G\n#SBATCH --time 05:00:00\n#SBATCH --ntasks=2\n')
+	ref.write('#SBATCH --mail-user=fab.alt@protonmail.com\n#SBATCH --mail-type=FAIL\n')
 	ref.write('module load cuda cudnn\nsource ~/env1/bin/activate\n')
         cwd = os.getcwd()
         ref.write('cd {}\n'.format(cwd))
