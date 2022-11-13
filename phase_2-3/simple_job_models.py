8d0047dcb03ed56d946c4054539e07d831ca5900
FebsN0 <72561622+FebsN0@users.noreply.github.com>
2022-11-12 20:11:21 -0700

    Update simple_job_models.py

--- phase_2-3/simple_job_models.py
+++ phase_2-3/simple_job_models.py
@@ -164,11 +164,10 @@ count = 1
 for i in range(len(all_hyperparas)):
     with open(SAVE_PATH+'/iteration_'+str(n_it)+'/simple_job/simple_job_'+str(count)+'.sh', 'w') as ref:
         ref.write('#!/bin/bash\n')
-#personalized
 	ref.write('#SBATCH --account=def-jtus\n#SBATCH --nodes=1\n')
-	ref.write('#SBATCH --gres=gpu:a100:1     #####  AWARE: MODIFY NAME GPU BASED ON THE USED CLUSTER #####\n')
-	ref.write('#SBATCH --cpus-per-task=5\n#SBATCH --mem-per-cpu=2G\n#SBATCH --time 05:00:00\n#SBATCH --ntasks=2\n')
-	ref.write('#SBATCH --mail-user=fab.alt@protonmail.com\n#SBATCH --mail-type=FAIL\n')
+	ref.write('#SBATCH --gres=gpu:1     #####  AWARE: MODIFY NAME GPU BASED ON THE USED CLUSTER #####\n')
+	ref.write('#SBATCH --cpus-per-task=8\n#SBATCH --mem-per-cpu=2G\n#SBATCH --time 05:00:00\n#SBATCH --ntasks=1\n')
+	ref.write('#SBATCH --mail-user=fab.alt@protonmail.com\n#SBATCH --mail-type=END\n')
 	ref.write('module load cuda cudnn\nsource ~/envDeepDock/bin/activate\n')
         cwd = os.getcwd()
         ref.write('cd {}\n'.format(cwd))
